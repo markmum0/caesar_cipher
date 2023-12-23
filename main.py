9702f66c8b4f4b8b361f26b07aed44ae02625e63
markmum0 <marckmumo@gmail.com>
2023-12-23 07:46:37 +0300

    latest commit. I have added the choice to users to either encrypt or decrypt.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 print('**** CAESARS CIPHER ****')
-# this is the word to be encrypted
-message = input('Enter the words to encrypt: ')
-# this is by how much we want to shift the letters across the alphabet key
-shift = int(input('Enter your desired shift on the alphabet: '))
-# the key
+
+# Asking whether the user wants to encrypt or decrypt
+user_choice = input('To Encrypt enter (e) / To decrypt enter (d): ')
+
+# The key
 alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-
-# encrypted_text = ''
 def encrypt(message, shift):
     encrypted_text = ''
     for char in message.lower():
@@ -15,19 +13,13 @@ def encrypt(message, shift):
             encrypted_text += char
         else:
             index = alphabet.find(char)
-            # print(index)
-            new_index = (int(index) + int(shift)) % len(alphabet)
-            # print(new_index)
+            new_index = (index + shift) % len(alphabet)
             encrypted_text += alphabet[new_index]
-    print(encrypted_text)
+    print('Encrypted text:', encrypted_text)
 
-encrypt(message, shift)
-
-encrypted_text = encrypt(message, shift)
-# this is the decryption function
-def decrypt(encrypted_text, shift):
+def decrypt(encrypted_message, shift):
     decrypted_text = ''
-    for char in encrypted_text:
+    for char in encrypted_message:
         if char == ' ':
             decrypted_text += char
         else:
@@ -36,5 +28,18 @@ def decrypt(encrypted_text, shift):
             decrypted_text += alphabet[new_index]
     print('Decrypted text:', decrypted_text)
 
-# Call the decrypt function
-decrypt(encrypted_text, shift)
+# Depending on user choice, perform encryption or decryption
+if user_choice.lower() == 'e':
+    # This is the word to be encrypted
+    message = input('Enter the words to encrypt: ')
+    # This is by how much we want to shift the letters across the alphabet key
+    shift = int(input('Enter your desired shift on the alphabet: '))
+    encrypt(message, shift)
+elif user_choice.lower() == 'd':
+    # This is the word to be decrypted
+    encrypted_text = input('Enter the words to decrypt: ')
+    # This is by how much we want to shift the letters across the alphabet key
+    shift = int(input('Enter your desired shift on the alphabet: '))
+    decrypt(encrypted_text, shift)
+else:
+    print('Invalid input. To Encrypt enter (e) / To decrypt enter (d)')
